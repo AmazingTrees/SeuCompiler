c8072b4acc23649efb99c2734e2bff09293f9488
Sihouette <seuweijiansun@gmail.com>
2018-05-17 20:57:27 +0800

    添加nfa.h和nfa.cpp

---
+++ SeuLex/nfa.cpp
@@ -0,0 +1,331 @@
+#include "nfa.h"
+
+/*
+ *   连接符, 对于两个Frag片段, 如果有连接符存在, 则进行连接操作
+ * 对于正则表达式来说, 需要选择一个不会被用到的字符
+ */
+#define LINK 01
+
+
+char *ReToNFA::ReTopost(char *re)
+{
+    int nalt, natom;
+    char * dst;
+    struct
+    {
+        int nalt;
+        int natom;
+    } paren[100], *p;
+
+    p = paren;
+    dst = buf;
+    nalt = 0;
+    natom = 0;
+
+    if(strlen(re) >= sizeof(buf) / 2)
+    {
+        return NULL;
+    }
+
+    for(; *re; re++)
+    {
+        switch(*re)
+        {
+
+            case '(':
+                if(natom > 1)
+                {
+                    --natom;
+                    *dst++ = LINK;
+                }
+
+                if(p >= paren + 100)
+                {
+                    return NULL;
+                }
+
+                p->nalt = nalt;
+                p->natom = natom;
+                p++;
+                nalt = 0;
+                natom = 0;
+                break;
+
+            case '|':
+                if(natom == 0)
+                {
+                    return NULL;
+                }
+
+                while(--natom > 0)
+                {
+                    *dst++ = LINK;
+                }
+
+                nalt++;
+                break;
+
+            case ')':
+                if(p == paren)
+                {
+                    return NULL;
+                }
+
+                if(natom == 0)
+                {
+                    return NULL;
+                }
+
+                while(--natom > 0)
+                {
+                    *dst++ = LINK;
+                }
+
+                for(; nalt > 0; nalt--)
+                {
+                    *dst++ = '|';
+                }
+
+                --p;
+                nalt = p->nalt;
+                natom = p->natom;
+                natom++;
+                break;
+
+            case '*':
+            case '+':
+            case '?':
+                if(natom == 0)
+                {
+                    return NULL;
+                }
+
+                *dst++ = *re;
+                break;
+
+            case '\\' : {
+                if(natom > 1)
+                {
+                    --natom;
+                    *dst++ = LINK;
+                }
+                *dst++ = *re;
+                re ++;
+                *dst++ = *re;
+                natom++;
+                break;
+            }
+
+            default:
+                if(natom > 1)
+                {
+                    --natom;
+                    *dst++ = LINK;
+                }
+
+                *dst++ = *re;
+                natom++;
+                break;
+        }
+    }
+
+    if(p != paren)
+    {
+        return NULL;
+    }
+
+    while(--natom > 0)
+    {
+        *dst++ = LINK;
+    }
+
+    for(; nalt > 0; nalt--)
+    {
+        *dst++ = '|';
+    }
+
+    *dst = 0;
+    return buf;
+}
+
+
+State *ReToNFA::postTonfa(char *postfix)
+{
+    char * p;
+    Frag stack[1000], *stackp, e1, e2, e;
+    State * start, *end;
+
+#define push(s) *stackp++ = s
+#define pop()   *--stackp
+
+    stackp = stack;
+
+    if(postfix == NULL)
+    {
+        return NULL;
+    }
+
+    for(p = postfix; *p; p++)
+    {
+        switch(*p)
+        {
+        case LINK:
+            e2 = pop();
+            e1 = pop();
+
+            if(e1.end->c == Split)
+            {
+                e1.end->out1 = e2.start;
+            }
+            else
+            {
+                e1.end->c = Split;
+                e1.end->out = e2.start;
+            }
+            push(Frag(e1.start, e2.end));
+            break;
+
+        case '?':
+            e = pop();
+            end = new State(Match, NULL, NULL);
+            start = new State(Split, e.start, end);
+            if (e.end->c == Split) {
+                e.end->out1 = end;
+            } else {
+                e.end->c = Split;
+                e.end->out = end;
+            }
+
+            push(Frag(start, end));
+            break;
+
+        case '|':
+            e1 = pop();
+            e2 = pop();
+            start = new State(Split, e1.start, e2.start);
+            end = new State(Match, NULL, NULL);
+            e1.end->c = Split;
+            e2.end->c = Split;
+            e1.end->out = end;
+            e2.end->out = end;
+            push(Frag(start, end));
+            break;
+
+        case '*':
+            e = pop();
+            end = new State(Match, NULL, NULL);
+            start = new State(Split, e.start, end);
+            e.end->c = Split;
+            e.end->out = e.start;
+            e.end->out1 = end;
+            push(Frag(start, end));
+            break;
+
+        case '+':
+            e = pop();
+            start = new State(Split, e.start, NULL);
+            end = new State(Match, NULL, NULL);
+            e.end->c = Split;
+            e.end->out = e.start;
+            e.end->out1 = end;
+            push(Frag(start, end));
+            break;
+
+        case '\\': {
+            p++;
+            end = new State(Match, NULL, NULL);
+            start = new State(*p, end, NULL);
+            char_set.insert(*p);
+            push(Frag(start, end));
+            break;
+        }
+
+        default:
+            end = new State(Match, NULL, NULL);
+            start = new State(*p, end, NULL);
+            char_set.insert(*p);
+            push(Frag(start, end));
+            break;
+        }
+    }
+
+    e = pop();
+
+    if(stackp != stack)      // 如果栈中仍然存在其余元素, 则不匹配
+    {
+        printf("error occuerd\n");
+        this->showNFA(e.start);
+        exit(1);
+    }
+
+
+    if(e.end->c == Match)
+    {
+        //e.end->out1 = end;
+        //printf ( "already matched\n" );
+        e.end->endFunc = this->func;
+    }
+    else
+    {
+        end = new State(Match, NULL, NULL);
+        end->endFunc = this->func;
+        e.end->out1 = end;
+        e.end = end;
+    }
+
+    nfa_s = e.start;
+    nfa_e = e.end;
+
+    return nfa_s;
+
+}
+
+/**
+ * 打印NFA
+ */
+void ReToNFA::showNFA(State *start)
+{
+    if(start == NULL ||
+            start->c == Match ||
+            haveTravel.find(start) != haveTravel.end())
+    {
+        return;
+    }
+
+    if(state2id.find(start) == state2id.end())
+    {
+        state2id.insert(std::make_pair(start, id));
+        id ++;
+    }
+
+    haveTravel.insert(start);
+
+    if(start->out != NULL)
+    {
+        if (haveTravel.find(start->out) == haveTravel.end()) {
+            state2id.insert(std::make_pair(start->out, id));
+            id++;
+        }
+        if (start->c < 256) {
+            printf("%d -> %c -> %d\n",
+                   state2id.at(start), start->c, state2id.at(start->out));
+        } else {
+            if (start->out->c == Match) {
+                printf("%d -> [] -> [%d]\n", state2id.at(start), state2id.at(start->out));
+            } else {
+                printf("%d -> [] -> %d\n", state2id.at(start), state2id.at(start->out));
+            }
+        }
+        showNFA(start->out);
+    }
+
+    if(start->out1 != NULL)
+    {
+        if (haveTravel.find(start->out1) == haveTravel.end()) {
+            state2id.insert(std::make_pair(start->out1, id));
+            id++;
+        }
+        printf("%d -> [] -> %d\n", state2id.at(start), state2id.at(start->out1));
+        showNFA(start->out1);
+    }
+}
---
+++ SeuLex/nfa.h
@@ -0,0 +1,170 @@
+#ifndef NFA_H_
+#define NFA_H_
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <set>
+#include <map>
+#include <algorithm>
+#include "state.h"
+
+using namespace std;
+class NFA {
+public:
+    virtual State *getStart() = 0;
+    virtual std::set<int> getCharSet() = 0;
+};
+
+/*
+ * 每条正则语句对应于一条NFA
+ *
+ */
+class ReToNFA: public NFA
+{
+public:
+    ReToNFA(char *str)
+    {
+        strcpy(this->re, str);
+    }
+
+    ReToNFA(char *str, string func)
+    {
+        strcpy(this->re, str);
+        this->func = func;
+    }
+
+    ~ReToNFA()
+    {
+        if (nfa_s == NULL) {
+            return;
+        }
+        haveTravel.clear();
+        free(nfa_s);
+        nfa_s = NULL;
+        nfa_e = NULL;
+    }
+
+    /**
+     * 析构对象时, 一个对象不可以重复删除
+     */
+    void free(State *start) {
+        if (start == NULL ||
+            haveTravel.find(start) != haveTravel.end() ) {
+            return;
+        }
+
+        haveTravel.insert(start);
+
+        free(start->out);
+        start->out = NULL;
+        free(start->out1);
+        start->out1 = NULL;
+
+        delete start;
+    }
+
+    void printNFA() {
+        haveTravel.clear();
+        this->showNFA(this->nfa_s);
+    }
+
+    void strToNFA() {
+        ReTopost(this->re);
+        postTonfa(this->buf);
+
+    }
+
+    State *getStart() {
+        return this->nfa_s;
+    }
+
+    std::set<int> getCharSet() {
+        return this->char_set;
+    }
+
+
+private:
+
+    /*
+     * 由后缀表达式形式转化为NFA
+     */
+    State *postTonfa(char * postfix);
+    void showNFA(State * start);
+
+    /**
+     * 此段代码来源:
+     * https://swtch.com/~rsc/regexp/
+     *
+     *
+     * Convert infix regexp re to postfix notation
+     * Insert . as explicit concatenation operator.
+     *
+     * 中缀转化为后缀, 同时插入'.'作为连接符
+     * 例如ab, 转化为后缀为'ab.'
+     *
+     * 字符具有高于运算符的优先级, "m|food"匹配"m"或是"food", 如果想要匹配
+     * "mood"或是"food", 需要使用"(m|f)ood"进行匹配.
+     * 上面的两个正则表达式需要以这样的方式进行传递: '\\+|\\-', '\\(|\\)' ,  使用两个转义'\'是由于c语言自身的原因, 本
+     * 身会进行一次转义, 因此, 我们需要自己来添加
+     */
+    char *ReTopost(char * re);
+
+    char buf[1024] = {0};
+    char re[1024] = {0};
+    string func;            // 该NFA所对应的终止状态的处理函数, 该NFA所对应的结局
+    State * nfa_s = NULL;   // 保存解析后的数据
+    State * nfa_e = NULL;   // 最终符合条件的状态
+    std::set<int> char_set; // 输入符号集合
+
+
+    int id = 0;
+    std::set<State *> haveTravel;
+    std::map<State *, int> state2id;
+};
+
+/*
+ *   该类主要用来合并NFA, 合并完成后的NFA仍然为NFA, 只是不同的NFA链所对应的最终处理函数不同,
+ */
+class NFAToLIST: public NFA {
+public:
+
+    State *nfa_s = NULL;
+    std::set<int> char_set;
+
+    std::set<State*> st_create; // 统计新建的资源, 为了最终释放
+
+    NFAToLIST() {}
+
+    ~NFAToLIST() {
+        for (auto state: st_create) {
+            delete state;
+        }
+        st_create.clear();
+    }
+
+    /**
+     * 合并操作, 主要用来合并NFA, 合并时, 新建一个新的起始节点, 再将两个NFA的字符集进行合并,
+     */
+    NFAToLIST* merge(ReToNFA *ReToNFA) {
+        State *create = new State(Split, ReToNFA->getStart(), nfa_s);
+        std::set<int> char_set1 = ReToNFA->getCharSet();
+
+        char_set.insert(char_set1.begin(), char_set1.end());
+        st_create.insert(create);
+        nfa_s = create;
+        return this;
+    }
+
+    State* getStart() {
+        return this->nfa_s;
+    }
+
+    std::set<int> getCharSet() {
+        return this->char_set;
+    }
+};
+
+#endif
